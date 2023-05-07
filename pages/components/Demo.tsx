e603230961449f09e5c7ff65c0102ae7df14314b
Ric-Lavers <ric_lavers@outlook.com>
2023-05-07 09:36:20 +0100

    add context

--- pages/components/Demo.tsx
+++ pages/components/Demo.tsx
@@ -1,53 +1,104 @@
 //@ts-nocheck
 
-import React, { useState, memo } from "react"
+import React, { useState, useCallback, useMemo, memo, useContext } from "react"
 /**
  * demostrate some uses of useState, and memoisation, and introduce the react profiler
  */
+
+const getLocalTime = () => new Date().toLocaleTimeString()
+
+const Context = React.createContext({
+  title: "Parent",
+})
 
 export const Demo = () => {
   const [list, s_list] = useState([1, 2, 3])
   const [title, s_title] = useState("Parent")
 
-  const add = () => {
-    s_list([...list, list.length + 1])
-  }
-  const reset = () => {
-    s_list([])
-  }
+  const add = useCallback(() => {
+    s_list((prevList) => [...prevList, prevList.length + 1])
+  }, [s_list])
+
+  const reset = useCallback(
+    function () {
+      s_list([])
+    },
+    [s_list]
+  )
+
+  const currentTime = useMemo(getLocalTime, [])
+  //
+
+  // new Date().toLocaleTimeString(), [])
 
   return (
-    <>
+    <Context.Provider value={{ title, list }}>
+      <Header />
       <input
         onChange={({ target: { value } }) => s_title(value)}
         value={title}
       />
-      <button onClick={add}>add</button>
-      <button onClick={reset}>reset</button>
 
-      <Parent list={list} />
-    </>
+      <h2>{title}</h2>
+      <SideBarData />
+      <Parent list={list} add={add} reset={reset} currentTime={currentTime} />
+      <Footer />
+    </Context.Provider>
   )
 }
 
+const Header = memo(() => {
+  return (
+    <header style={{ border: "1px solid pink", width: "100%" }}>Header</header>
+  )
+})
+
+const Footer = memo(() => {
+  return (
+    <footer style={{ border: "1px solid orange", width: "100%" }}>
+      Footer
+    </footer>
+  )
+})
+const SideBar = memo(({ title }) => {
+  console.log("rendering side bar")
+  return (
+    <aside style={{ border: "1px solid lime", height: "50vh" }}>
+      SideBar - {title}
+    </aside>
+  )
+})
+
+const withTitleData = (Component) => () => {
+  const { title } = useContext(Context)
+
+  return <Component {...{ title }} />
+}
+
+const SideBarData = withTitleData(SideBar)
+
 const Parent: React.FC<{ title: string }> = memo(
-  ({ children, title, list }) => {
+  ({ children, list, add, reset, currentTime }) => {
+    const { title } = useContext(Context)
     return (
       <div style={{ padding: 4, border: "1px solid orange" }}>
-        <h2>{title}</h2>
+        <button onClick={add}>add</button>
+        <button onClick={reset}>reset</button>
+        <p>{title}</p>
         <div style={{ padding: 4, border: "1px solid lime" }}>
           {list.map((itm) => (
             <Child />
           ))}
         </div>
+        <date>{currentTime}</date>
       </div>
     )
-  },
-  (oldProps, newProps) => {
-    return true
-    console.log({ oldProps, newProps })
-    return false
   }
+  // (oldProps, newProps) => {
+  //   return true
+  //   console.log({ oldProps, newProps })
+  //   return false
+  // }
 )
 
 const Child: React.FC = () => {
